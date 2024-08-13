b58dabf51f80be62ae96e3514d4c79824f5223d9
patrickjm <patrick@moriarty.dev>
2024-08-12 22:12:21 -0500

    working but need flash-attn and trim json output

--- predict.py
+++ predict.py
@@ -1,35 +1,56 @@
 import json
 from typing import List
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from cog import BasePredictor, Input
 import os
 
 class Predictor(BasePredictor):
     def setup(self):
         # Path to local files
-        model_path = "./"
+        model_path = "./weights"
 
-        # Load the model from local files
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        custom_files = {
+            "configuration_phi3": os.path.join(model_path, "configuration_phi3.py"),
+            "modeling_phi3": os.path.join(model_path, "modeling_phi3.py")
+        }
+        
+        # Load the configuration
+        config = AutoConfig.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+            local_files_only=True,
+        )
+        
+        # Load the model and tokenizer
         self.model = AutoModelForCausalLM.from_pretrained(
             model_path,
-            device_map="auto",
+            config=config,
             trust_remote_code=True,
+            torch_dtype=torch.float16,
+            # attn_implementation="flash_attention_2",
             local_files_only=True
-        ).eval()
-
-        # Load the tokenizer from local files
+        ).to(self.device)
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_path,
-            trust_remote_code=True,
             local_files_only=True
         )
 
     def predict(
         self,
-        text: str = Input(description="Input text to extract triplets from"),
-        entity_types: List[str] = Input(description="List of entity types to extract", default=["LOCATION", "POSITION", "DATE", "CITY", "COUNTRY", "NUMBER"]),
-        predicates: List[str] = Input(description="List of predicates to extract", default=["POPULATION", "AREA"])
+        text: str = Input(
+            description="Input text to extract triplets from"
+        ),
+        entity_types: List[str] = Input(
+            description="List of entity types to extract",
+            default=["LOCATION", "POSITION", "DATE", "CITY", "COUNTRY", "NUMBER"]
+        ),
+        predicates: List[str] = Input(
+            description="List of predicates to extract",
+            default=["POPULATION", "AREA"]
+        )
     ) -> str:
         input_format = """Perform Named Entity Recognition (NER) and extract knowledge graph triplets from the text. NER identifies named entities of given entity types, and triple extraction identifies relationships between entities using specified predicates.
       
--- script/download-weights.sh
+++ script/download-weights.sh
@@ -5,16 +5,23 @@ echo "Downloading..."
 source .env
 
 download_weights() {
-  include=$1
-  huggingface-cli download sciphi/triplex \
-    --revision d3c3a297391cb5609ff7f5350c426ed0908112da \
+  model=$1
+  revision=$2
+  include=$3
+  huggingface-cli download $model \
+    --revision $revision \
     --token $HUGGINGFACE_TOKEN \
-    --local-dir ./ \
+    --local-dir ./weights \
     --include="*.$include"
 }
 
-download_weights "json"
-download_weights "safetensors"
-download_weights "model"
+triplex_revision="d3c3a297391cb5609ff7f5350c426ed0908112da"
+ms_phi_revision="d548c233192db00165d842bf8edff054bb3212f8"
+
+download_weights "sciphi/triplex" "$triplex_revision" "json"
+download_weights "sciphi/triplex" "$triplex_revision" "safetensors"
+download_weights "sciphi/triplex" "$triplex_revision" "model"
+download_weights "microsoft/Phi-3-mini-128k-instruct" "$ms_phi_revision" "py"
+
 
 echo "Done!"
